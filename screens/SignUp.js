528d79bc77948bc32cd14f3db3fc24a6a18bd2fd
bigyankarki <b.gyankarki@gmail.com>
2018-12-02 15:43:06 -0500

    added new merchant form

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
-import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+import { Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
 import firebase from 'react-native-firebase';
 
@@ -8,25 +8,64 @@ export default class SignUp extends React.Component {
   constructor() {
     super();
     this.state = {
-      userInfo : {}
+      userInfo : {},
+      name : '',
+      type : '',
+      phone: '',
+      address: ''
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  async componentWillMount() {
+  static navigationOptions = {
+    title: 'Sign up',
+  };
+
+  componentWillMount() {
     const { navigation } = this.props;
     const info = navigation.getParam('userInfo', 'NO-ID');
     this.setState({userInfo : info})
   }
 
+  handleInputChange = (input, text) => {
+    this.setState({[input] : text})
+    console.log(this.state.name);
+  }
+
+
+
+
+
   render() {
     return (
-        <View style={styles.container}>
-          <Text>Hi {this.state.userInfo.user.givenName}!</Text>
-          <Text>Looks like you are new to Paird.</Text>
-          <Text>Please complete the form to signup.</Text>
-          <FormLabel>Name</FormLabel>
-          <FormInput onChangeText={alert("gi")}/>
-          <FormValidationMessage>Error message</FormValidationMessage>
+        <View >
+
+          <View>
+            <Text>Hi {this.state.userInfo.user.givenName}!</Text>
+            <Text>Looks like you are new to Paird.</Text>
+            <Text>Please complete the form to signup.</Text>
+          </View>
+
+          <View>
+            <FormLabel>Business Name</FormLabel>
+            <FormInput onChangeText={text => this.handleInputChange('name', text)} />
+
+            <FormLabel>Business Type</FormLabel>
+            <FormInput onChangeText={(text) => console.log(text)}/>
+
+            <FormLabel>Business Phone Number</FormLabel>
+            <FormInput onChangeText={(text) => console.log(text)}/>
+
+            <FormLabel>Business Address</FormLabel>
+            <FormInput onChangeText={(text) => console.log(text)}/>
+
+            <Button
+            raised
+            icon={{name: 'send'}}
+            title='Submit'
+            onPress={console.log(this.state.businessInfo.name)} />
+
+          </View>
 
         </View>
     );
