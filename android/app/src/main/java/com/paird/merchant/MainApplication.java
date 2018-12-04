e823a0e0625f56b6acbd072ec586b528768ca96e
bigyankarki <b.gyankarki@gmail.com>
2018-12-04 16:48:27 -0500

    updated modal

--- android/app/src/main/java/com/paird/merchant/MainApplication.java
+++ android/app/src/main/java/com/paird/merchant/MainApplication.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -47,6 +48,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
         new MainReactPackage(),
+            new ImagePickerPackage(),
             new VectorIconsPackage(),
         new RNGoogleSigninPackage(),
         new RNGestureHandlerPackage(),
--- screens/ModalScreen.js
+++ screens/ModalScreen.js
@@ -1,22 +1,57 @@
 import React from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, ScrollView } from 'react-native';
 import { Card, Button, ListItem, Icon, FormLabel, FormInput, Divider } from 'react-native-elements';
+import ImagePicker from 'react-native-image-picker';
 
 export default class ModalScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item_image_uri : '',
+      item_name : '',
+      item_description : '',
+      item_price : ''
+    };
+    this.pickImage = this.pickImage.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  pickImage = () => {
+    ImagePicker.showImagePicker({title: "Pick an Image", maxWidth: 800, maxHeight: 600}, res => {
+     if (res.didCancel) {
+       console.log("User cancelled!");
+     } else if (res.error) {
+       console.log("Error", res.error);
+     } else {
+       this.setState({
+         item_image_uri: res.uri
+       });
+     }
+   });
+  }
+
+  handleInputChange = (input, text) => {
+    this.setState({[input] : text})
+  }
+
+
+
   render() {
     return (
 
+      <ScrollView>
         <Card title="Add an item" style={styles.container}>
 
-            <Icon
-              raised
-              name='add'
-              color='#f50'
-              containerStyle ={{marginLeft:160, marginTop:100, marginBottom:100}}
-              onPress={() => console.log("add an image.")} />
+        {this.state.item_image_uri == '' ?
+          <Icon
+            raised
+            name='add'
+            color='#f50'
+            containerStyle ={{marginLeft:160, marginTop:100, marginBottom:100}}
+            onPress={this.pickImage} />
+        : <Image style={{width: 350, height: 350}} source={{uri:  this.state.item_image_uri}}/>}
 
               <Divider style={{ backgroundColor: 'gray' }} />
-
 
               <FormLabel>Item Name</FormLabel>
               <FormInput containerStyle={styles.inputField} onChangeText={text => console.log(text)}/>
@@ -25,7 +60,7 @@ export default class ModalScreen extends React.Component {
               <FormInput multiline={true} containerStyle={styles.inputField} onChangeText={text => console.log(text)}/>
 
               <FormLabel>Item Price</FormLabel>
-              <FormInput containerStyle={styles.inputField} onChangeText={text => console.log(text)}/>
+              <FormInput keyboardType='decimal-pad' containerStyle={styles.inputField} onChangeText={text => console.log(text)}/>
 
               <Button
                 icon={{name: 'send'}}
@@ -35,7 +70,15 @@ export default class ModalScreen extends React.Component {
                 onPress={() => this.props.navigation.goBack()}
                 />
 
+              <Button
+                icon={{name: 'cancel'}}
+                backgroundColor='#03A9F4'
+                buttonStyle={{borderRadius: 50, marginLeft: 0, marginRight: 0, marginBottom: 0, marginTop: 20}}
+                title='Dismiss'
+                onPress={() => this.props.navigation.goBack()}
+                />
         </Card>
+        </ScrollView>
 
     );
   }
@@ -44,7 +87,7 @@ export default class ModalScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0,
+    flex: 1,
      justifyContent: 'center',
      alignItems: 'center',
      backgroundColor: '#F5FCFF',
